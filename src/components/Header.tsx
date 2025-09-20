b8b69cc0bb0d3125e9e4791e90099bbca114c0ae
D-Karak <dibyendu.karak05@gmail.com>
2025-09-21 00:12:24 +0530

    initial commit for developing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,107 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from '../../images/logo.png'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import SearchBar from './SearchBar'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../images/logo.png";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import SearchBar from "./SearchBar";
 const Header = () => {
   return (
-    <header className='border-b bg-white'>
-        <div className='flex flex-col lg:flex-row items-center gap-4 p-4'>
-            <div className='flex items-center justify-between w-full lg:w-auto'>
-                <Link href='/' className="font-bold shrink-0">
-                    <Image
-                    src={logo}
-                    alt="EventBay Logo"
-                    width={100}
-                    height={100}    
-                    className="w-20 lg:w-18"
-                    ></Image>
-                </Link>
-                <div className='lg:hidden'>
-                    <SignedIn>
-                        <UserButton/>
-                    </SignedIn>
+    <header className="border-b bg-white">
+      <div className="flex flex-col lg:flex-row items-center gap-4 p-4">
+        <div className="flex items-center justify-between w-full lg:w-auto">
+          <Link href="/" className="font-bold shrink-0">
+            <Image
+              src={logo}
+              alt="EventBay Logo"
+              width={100}
+              height={100}
+              className="w-20 lg:w-18"
+            ></Image>
+          </Link>
+          <div className="lg:hidden">
+            <SignedIn>
+            <div className="inline-block ml-6 mr-5">
+              <UserButton
+                appearance={{
+                   elements: {
+                    rootBox: {
+                      width: "38px",
+                      height: "38px",
+                    },
+                    avatarBox: {
+                      width: "38px",
+                      height: "38px",
+                    },
+                  },
+                }}
+              />
+              </div>
+            </SignedIn>
 
-                    <SignedOut>
-                        <SignInButton>
-                            <button className='text-sm text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-secondary hover:border-accent hover:text-primary cursor-pointer'>Sign In</button>
-                        </SignInButton>
-                    </SignedOut>
-                </div>
-            </div>
-            {/* searchBar */}
-            <div className='w-full lg:max-w-2xl'>
-                <SearchBar/>
-            </div>
+            <SignedOut>
+              <SignInButton>
+                <button className="text-sm text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-secondary hover:border-accent hover:text-primary cursor-pointer">
+                  Sign In
+                </button>
+              </SignInButton>
+            </SignedOut>
+          </div>
         </div>
-    </header>
-  )
-}
+        {/* searchBar */}
+        <div className="w-full lg:max-w-2xl">
+          <SearchBar />
+        </div>
 
-export default Header
+        {/* Desktop action buttons */}
+        <div className="hidden  ml-auto lg:flex lg:items-center lg:justify-center">
+          <SignedIn>
+            <Link href="/mytickets">
+              <button className="text-sm text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-primary font-medium hover:text-white cursor-pointer transition-colors duration-200">
+                My Tickets
+              </button>
+            </Link>
+            <Link href="seller">
+              <button className="ml-4 text-sm text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-primary font-medium hover:text-white cursor-pointer transition-colors duration-200">
+                Sell Tickets
+              </button>
+            </Link>
+            <div className="inline-block ml-6 mr-5">
+              <UserButton
+                appearance={{
+                  elements: {
+                    rootBox: {
+                      width: "38px",
+                      height: "38px",
+                    },
+                    avatarBox: {
+                      width: "38px",
+                      height: "38px",
+                    },
+                  },
+                }}
+              />
+            </div>
+          </SignedIn>
+        </div>
+        {/* Mobile action buttons */}
+        <SignedIn>
+            <div className="flex lg:hidden w-full gap-3 items-center">
+                <Link href="/mytickets" className="flex-1">
+              <button className="text-sm w-full text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-primary font-medium hover:text-white cursor-pointer transition-colors duration-200">
+                My Tickets
+              </button>
+            </Link>
+            <Link href="seller" className="flex-1">
+              <button className="text-sm w-full text-foreground border-2 border-primary py-2 px-3 rounded hover:bg-primary font-medium hover:text-white cursor-pointer transition-colors duration-200">
+                Sell Tickets
+              </button>
+            </Link>
+            </div>
+        </SignedIn>
+
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,13 +6,13 @@ const SearchBar = () => {
     <div>
         <Form action="/search" className='relative '>
         <input type="text"
-        placeholder='Search your fav events...'
+        placeholder='Search your favourite events, artists, venues. . .'
         name='q'
-        className='w-full border-2 border-gray-300 rounded-full py-2 pl-12 pr-4 focus:outline-none shadow-sm focus:ring-2 focus:ring-blue-500 transition-all duration-200'
+        className='w-full border-2 border-gray-300 rounded-lg py-2 pl-12 pr-4 focus:outline-none shadow-sm focus:ring-2 focus:ring-accent transition-all duration-200'
         />
         <Search className='absolute left-4 top-1/2 -translate-y-1/2 text-gray-500 w-5 y-5'/>
         <button type='submit'
-         className='absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer bg-accent text-white rounded-lg text-sm font-medium hover:bg-blue-700 transition-colors duration-200 px-4 py-1.5'>Search</button>
+         className='absolute top-1/2 right-0 -translate-y-1/2 h-full cursor-pointer bg-primary text-white rounded-r-lg text-sm font-medium hover:bg-accent transition-colors duration-200 px-4 py-1.5'>Search</button>
         </Form>
     </div>
   )
