9b0beebfeb62b1ef7f1c7a4e0f1f53e88c9c5536
D-Karak <dibyendu.karak05@gmail.com>
2025-09-27 23:37:58 +0530

    sync user data in database from clerk

--- src/components/syncUserWithConvex.tsx
+++ src/components/syncUserWithConvex.tsx
@@ -3,17 +3,17 @@ import {useUser} from "@clerk/nextjs"
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { useEffect } from "react";
-import { uptime } from "process";
 const syncUserWithConvex = () => {
     const {user} = useUser();
-    // const updatUser=useMutation(api.users.updateUser())
+    //this is how to call convex mutation in react which is useMutation hook use for calling mutation, mutation is used for creating, updating, deleting data
+    const updateUser=useMutation(api.user.updateUser)
 
     useEffect(() => {
         if(!user) return;
         const syncUser=async()=>{
             try{
                 await updateUser({
-                    userid:user.id,
+                    userId:user.id,
                     email:user.emailAddresses[0]?.emailAddress||"",
                     name:user.fullName||""
                 })
